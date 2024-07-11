52e15195fb4ce4d23501eebcd6cb5cdfbaca54aa
azmyar <azmy.arya.rizaldi@gmail.com>
2024-07-11 21:23:44 +0700

    feat: setting up cors

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,27 @@
 require("dotenv").config();
 import express from "express";
+import cors from "cors";
 
 const PORT: string = process.env.PORT!;
 
 const app = express();
+
+const whitelist: string[] = [];
+
+const corsOptions: cors.CorsOptions = {
+  origin: function (
+    origin: string | undefined,
+    callback: (error: Error | null, allow?: boolean) => void
+  ) {
+    if (!origin || whitelist.length === 0 || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
