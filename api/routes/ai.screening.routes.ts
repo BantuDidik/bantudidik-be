623f07f0c781f05f44897bd5682887a6af4e73e5
azmyar <azmy.arya.rizaldi@gmail.com>
2024-07-13 05:37:12 +0700

    feat: temporarely disabling AI verification

--- api/routes/ai.screening.routes.ts
+++ api/routes/ai.screening.routes.ts
@@ -1,9 +1,8 @@
-import { verifyToken } from "../middleware/verifyToken";
 import { execute } from "../services/ai.screening.service";
 import express from "express";
 
 const router = express.Router();
 
-router.post("/:idFunding", verifyToken, execute);
+router.post("/:idFunding", execute);
 
 module.exports = router;
